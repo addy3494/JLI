74bcccd4e41578317533203a6e8ce1f6f053a4eb
gh0s1 <adithya3494@gmail.com>
2021-09-18 04:59:34 +0530

    feat:(range delete and output all connections strings to path)

--- jc/__init__.py
+++ jc/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __author__ = 'P S, Adithya (adithya3494@gmail.com)'
 __license__ = 'MIT'
 
--- jc/jc.py
+++ jc/jc.py
@@ -7,6 +7,7 @@ import argparse
 from pyfzf.pyfzf import FzfPrompt
 from pathlib import Path
 
+
 # globals
 HEADER = '\033[95m'
 OKBLUE = '\033[94m'
@@ -25,8 +26,15 @@ def parse_args(*args, **kwargs):
     p_group.add_argument('-a', '--add', help="Add new connection entry", action="store_true")
     p_group.add_argument('-c', '--connect', help="Initate a intertactive connection menu", action="store_true")
     p_group.add_argument('-d', '--delete', help="Delete new connection entry", action="store_true")
-    p_group.add_argument('-l', '--list', help="List all available entries", action="store_true")
+    p_group.add_argument('-s', '--show', help="List all available entries", action="store_true")
+    p_group.add_argument('-o', '--output', help="Write out all connection strings", action="store_true")
     return parser.parse_args()
+
+
+def init(cursor):
+    cursor.execute(
+        """CREATE TABLE if not exists main(environment text,hostname text,ip_address real,username text,
+        password text)""")
 
 
 def initialize_db():
@@ -37,12 +45,6 @@ def initialize_db():
     cursor = connect.cursor()
     init(cursor)
     return [connect, cursor]
-
-
-def init(cursor):
-    cursor.execute(
-        """CREATE TABLE if not exists main(environment text,hostname text,ip_address real,username text,
-        password text)""")
 
 
 def column_header(cursor):
@@ -76,15 +78,15 @@ def add(cursor):
     print('{} *>  Do you have a file to load ?{}'.format(BOLD, ENDC))
     dec = input('{} *> [Y/N] :{} '.format(BOLD, ENDC))
     if dec.upper() == 'N':
-        print(" *>  Enter the details in '{}{}{}' format".format(BOLD, header, ENDC))
-        a_inpt = [input('  *> INPUT: ').split(',')]
+        print(" *>  Enter the details in '{}{}{}' format".format(BOLD,','.join(header), ENDC))
+        a_inpt = [input(' *> INPUT: ').split(',')]
     else:
         print('{} *> /path/to/file ?{}'.format(BOLD, ENDC))
         try:
             with open(input(), 'r') as file:
-                a_inpt = [line.strip('\n').split(',') for line in file if not line.startswith('#') or line.startswith('\n')]
-        except:
-            print('{} *> {}file not found{}'.format(BOLD,FAIL,ENDC))
+                a_inpt = [line.strip('\n').split(',') for line in file]
+        except Exception:
+            print('{} *> {}file not found{}'.format(BOLD, FAIL, ENDC))
             exit(1)
     data = [tuple(d) for d in a_inpt]
     print('{} *> The below info will be added, proceed ?{}\n'.format(BOLD, ENDC))
@@ -98,15 +100,16 @@ def connect(cursor):
     try:
         cursor.execute("SELECT rowid, * from main")
         data = cursor.fetchall()
-        conn_data = fzf.prompt(data)[0].translate(str.maketrans({"(": "", ")": "", "'": "", " ": ""})).split(',')
+        conn_data = fzf.prompt(data)[0].translate(str.maketrans(
+            {"(": "", ")": "", "'": "", " ": ""})).split(',')
         ssh_command = "ssh" + " " + conn_data[4] + "@" + conn_data[3]
         print("{} *> Executing {}'{}'{}".format(BOLD, OKCYAN, ssh_command, ENDC))
         os.system(ssh_command)
-    except:
-        print('{} *> {}connection request terminated{}'.format(BOLD, WARNING, ENDC))
+    except Exception:
+        terminate()
 
 
-def list(cursor) -> str:
+def show(cursor) -> str:
     header = column_header(cursor)
     header.insert(0, 'ID')
     separator = column_separator(header)
@@ -116,11 +119,52 @@ def list(cursor) -> str:
 
 
 def delete(cursor):
-    print(" *> Enter the rowid's to drop in {}id1,id2{} format".format(BOLD, ENDC))
-    d_inpt = input('INPUT: ').split(',')
-    for row_id in d_inpt:
-        cursor.execute('''DELETE from main WHERE rowid=? ''', (row_id,))
-    print("{} *> '{}' drop(s) {}successful{}".format(BOLD, len(d_inpt), OKGREEN, ENDC))
+    header = column_header(cursor)
+    header.insert(0, 'ID')
+    separator = column_separator(header)
+    print(" *> Enter the rowid's to drop in {}id1,id2 or range:'id3_id6'{} format".format(BOLD, ENDC))
+    d_inpt = input(' *> INPUT: ').split(',')
+    rows = []
+    try:
+        for n in d_inpt:
+            if '_' in n:
+                if int(n.split('_')[1]) > int(n.split('_')[0]):
+                    [rows.append(str(_n)) for _n in range(int(n.split('_')[0]), int(n.split('_')[1]) + 1)]
+                else:
+                    [rows.append(str(_n)) for _n in range(int(n.split('_')[0]), int(n.split('_')[1]) - 1, -1)]
+            else: rows.append(n)
+        data = []
+        for row_id in rows:
+            cursor.execute("""SELECT rowid, * from main WHERE rowid = ?""", (row_id,),)
+            data.append(cursor.fetchall()[0])
+    except IndexError:
+        print("\n{} *> {}Invalid range, use '-s' for info{}".format(BOLD, WARNING, ENDC))
+        exit(1)
+    print('{} *> The below info will be Deleted, proceed ?{}\n'.format(BOLD, ENDC))
+    pretty_print(data, header, separator)
+    dec = input('{}\n *> [Y/N] :{} '.format(BOLD, ENDC))
+    if dec.upper() == 'Y':
+        for row_id in rows:
+            cursor.execute(
+                '''DELETE from main WHERE rowid=? ''', (row_id,))
+        print("{} *> '{}' drop(s) {}successful{}".format(BOLD,len(rows), OKGREEN, ENDC))
+    else:
+        terminate()
+
+
+def output(cursor):
+    cursor.execute("SELECT * from main")
+    data = cursor.fetchall()
+    dec = input('{}\n *> /path/to/write ?: default will be current working directory:{} '.format(BOLD, ENDC))
+    dec if dec else './'
+    with open(os.path.join(f'{dec}','jc.out'), 'w', encoding='utf-8') as f:
+        for conn in data:
+            f.write(','.join(conn) + '\n')
+
+
+def terminate():
+    print('\n{} *> {}operation terminate{}'.format(BOLD, WARNING, ENDC))
+    exit(1)
 
 
 def conn_close(connect, cursor):
